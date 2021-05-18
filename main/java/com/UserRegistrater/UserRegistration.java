ad32fb5b68abba7b74daed6be444b8740adcafb0
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-19 00:13:10 +0530

    Added: code to verify all sample emails

--- main/java/com/UserRegistrater/UserRegistration.java
+++ main/java/com/UserRegistrater/UserRegistration.java
@@ -5,18 +5,19 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     static Scanner sc = new Scanner(System.in);
-    public static void validation(String firstName, String lastName, String email, String mobile, String password ) {
-        // First Name and Last Name Validation
+    public static void validation(String firstName, String lastName, String email, String mobile, String password) {
+        // First Name and Last Name comes with restriction such as first letter should be upper case and with minimum 3 characters
         System.out.println("First Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
         System.out.println("Last Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
-        // Email validation
-        System.out.println("E-Mail: " + Pattern.matches("[a-zA-Z-\\+]+(\\.[a-zA-Z0-9-]+)*@[A-Za-z-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", email));
-        // PhoneNumber validation
-        System.out.println("Mobile Number: " + Pattern.matches("^[9][1][\s][6-9]{1}[0-9]{9}$", mobile));
-        // Password validation (Rule 4: one special character should be there)
-        System.out.println("Password: " + Pattern.matches("^(?=.*[\\@\\#\\$\\%\\&\\_\\,\\.])(?=.*[0-9])[a-zA-Z0-9]{7,}$", password));
+        //Email validation of all sample emails
+        System.out.println("E-Mail: " + Pattern.matches("^abc[a-zA-Z0-9.+-]*@[a-z]*[.][a-z]{2,5}[.,a-z]{0,5}", email));
+        // 91 followed by space followed by 10 digit number
+        System.out.println("Mobile Number: " + Pattern.matches("^[9][1][\s][6-9][0-9]{9}$", mobile));
+        // minimum 8 characters required
+        System.out.println("Password: " + Pattern.matches("^(?=.*[\\@\\#\\$\\%\\&\\_\\,\\.])(?=.*[A-Z])(?=.*[0-9]).{8,}$", password));
     }
     public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Program");
         System.out.println("Enter a valid first name");
         String firstName = sc.nextLine();
         System.out.println("Enter a valid last name");
@@ -25,7 +26,7 @@ public class UserRegistration {
         String email = sc.nextLine();
         System.out.println("Enter a valid mobile number starting with 91 followed by a space and 10 digit number");
         String mobile = sc.nextLine();
-        System.out.println("Enter the password and it should be minimum of 8 characters");
+        System.out.println("Enter a valid password with minimum 8 characters");
         String password = sc.nextLine();
         validation(firstName, lastName, email, mobile, password);
 
